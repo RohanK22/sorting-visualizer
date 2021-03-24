5286c1ae07d783f1a582d733875f177de38f4977
RohanK22 <rohank2205@gmail.com>
2021-03-24 23:55:24 +0530

    removed unwanted stuff and finished insertion sort

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,13 @@
 import SortingVisualizer from './SortingVisualizer';
-import Toolbar from './Toolbar';
 import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-            selectedMenu: null,
-            finishedSorting: null,
-      };
-    
-  }
-
-  changeState(selectedM, finSort) {
-    this.setState({selectedMenu: selectedM, finishedSorting: finSort});
-    
-  }
-
+  
   render() {
-    console.log("Re rendering with :" + this.state.selectedMenu);
     return (
       <div className="App">
-        <Toolbar changeState={this.changeState.bind(this)} finishedSorting={this.state.finishedSorting} selectedMenu={this.state.selectedMenu}/>
-        <SortingVisualizer selectedMenu={this.state.selectedMenu} finishedSorting={this.state.finishedSorting}/>
+        <SortingVisualizer />
       </div>
     );
   }
--- client/src/SortingVisualizer.js
+++ client/src/SortingVisualizer.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const NUMBER_OF_LIST_ITEMS = 30;
-const PADDING = 3;
+const NUMBER_OF_LIST_ITEMS = 50;
+const PADDING = 1;
 const BAR_WIDTH  = window.innerWidth/NUMBER_OF_LIST_ITEMS - 2 * PADDING;
 const MAX_BAR_HEIGHT = window.innerHeight / 1.5;
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
-        
         super(props);
         let l = [];
         for(let i=0; i<NUMBER_OF_LIST_ITEMS; i++) {
@@ -15,42 +14,110 @@ export default class SortingVisualizer extends React.Component {
         }
         this.state = {
             list: l,
+            selectedMenu: null,
         }
+        this.generateRandomNumbers = this.generateRandomNumbers.bind(this);
+        this.changeMenu = this.changeMenu.bind(this);
+        this.generateRandomNumbers = this.generateRandomNumbers.bind(this);
+        this.bubbleSort = this.bubbleSort.bind(this);
     }
 
-    
+    generateRandomNumbers() {
+        let l = [];
+        for(let i=0; i<NUMBER_OF_LIST_ITEMS; i++) {
+            l.push(randomNum(MAX_BAR_HEIGHT));
+        }
+        this.setState({list:l});
+    }
 
-    componentDidMount() {
+    async bubbleSort() {
         let l = this.state.list;
-            let t;
-            for(let i = 0; i< l.length; i++) {
-                for(let j = 0; j< l.length - 1 - i; j++) {
-                    if(l[j] > l[j+1]) {
-                        t = l[j];
-                        console.log("M");
-                        l[j] = l[j+1];
-                        l[j+1] = t;
-                    }
+        const bars = document.getElementsByClassName("bar");
+        let t;
+        for(let i = 0; i< l.length; i++) {
+            for(let j = 0; j< l.length - 1 - i; j++) {
+                if(l[j] > l[j+1]) {
+                    t = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = t;
+                }
+                bars[j+1].style.backgroundColor = 'red';
+                bars[j].style.backgroundColor = 'red';
+                await timeout(0.1);
+                bars[j+1].style.backgroundColor = 'blue';
+                bars[j].style.backgroundColor = 'blue';
+                this.setState({list: l});
+            }
+        }  
+    }
+
+    async insertionSort() {
+        let l = this.state.list;
+        const bars = document.getElementsByClassName("bar");
+        let t;
+        for(let i = 0; i< l.length; i++) {
+            for(let j = 0; j< l.length - 1 - i; j++) {
+                if(l[j] > l[j+1]) {
+                    t = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = t;
                     
                 }
-            }      
+                bars[j+1].style.backgroundColor = 'red';
+                bars[j].style.backgroundColor = 'red';
+                await timeout(0.1);
+                bars[j+1].style.backgroundColor = 'blue';
+                bars[j].style.backgroundColor = 'blue';
+                this.setState({list: l});
+            }
+        }  
     }
-    
+
+    changeMenu(s) {
+        this.setState({selectedMenu: s});
+        this.render();
+    }
+
     render() {
         let bars = this.state.list.map((item, index) => {
             let divStyle = {
-                background: 'red',
                 height: item + "px",
                 margin: PADDING + "px",
                 width: BAR_WIDTH,
+                background: 'blue',
                 float: "left",
+                border: "1px",
+                "border-radius": "10px"
             };
             return (
-                <div style={divStyle} key={index}></div>
+                <div className="bar" style={divStyle} key={index}></div>
             );
         });
         return (
             <div className="SortingVisualizer">
+                <div className="Toolbar">
+                    <div className="ToolbarItem">
+                        <div className="ToolbarTitle">
+                            SortingVisualizer
+                        </div>
+                    </div>
+                    <div className="ToolbarItem">
+                        <button type="button" className="ToolBarItem" onClick={() => {
+                            console.log("bubble");
+                            this.bubbleSort();
+                            this.changeMenu("bubble");
+                        }}>Bubble Sort</button>
+                    </div>
+                    <div className="ToolbarItem">
+                        Selected: {this.state.selectedMenu}
+                    </div>
+                    <div className="ToolbarItem">
+                        <button type="button" className="ToolBarItem" onClick={() => {
+                            this.generateRandomNumbers();
+                        }}>Generate Array</button>
+                    </div>
+                    
+                </div>
                 {bars}
             </div>
         );
@@ -61,4 +128,8 @@ export default class SortingVisualizer extends React.Component {
 
 function randomNum(i) {
     return Math.floor(Math.random() * (i+1));
+}
+
+function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
 }
